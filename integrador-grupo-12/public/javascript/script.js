c7f5a0adabb75f9d530a44ac472dc7d25022bfea
lssbustamante <luissebastianbustamante@gmail.com>
2021-08-06 14:01:44 -0300

    termino navegacion, form register

--- integrador-grupo-12/public/javascript/script.js
+++ integrador-grupo-12/public/javascript/script.js
@@ -1,13 +1,28 @@
-// Button Hamburguer
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function(){
-    console.log('click hamburger');
+  console.log('click hamburger');
 
-    if (btnHamburger.classList.contains('open')) {
-       btnHamburger.classList.remove('open');
-    } else {
-       btnHamburger.classList.add('open');
-    }
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
     
+  }
+  else { // Open Hamburger Menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+
+  }  
 });
