d842162b9c20a0e733f14d3251ba98f3fc5012d8
lssbustamante <luissebastianbustamante@gmail.com>
2021-08-24 08:10:02 -0300

    CRUD admin

--- integrador-grupo-12/controllers/adminController.js
+++ integrador-grupo-12/controllers/adminController.js
@@ -1,10 +1,46 @@
 const path = require('path');
 const fs = require('fs');
-let products = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')));
+
 const adminController = {
     index: (req,res) => {
-        res.render(path.resolve(__dirname,'../views/admin/admin'), {products});
+        let products = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')));
+        res.render(path.resolve(__dirname,'../views/admin/admin'), {title: "Administrar", products});
     },
+    create: (req,res) => {
+        let products = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')));
+        res.render(path.resolve(__dirname, '../views/admin/create'), {title: "Crear"});
+    },
+    save: (req,res) => {
+        let products = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')));
+        let lastProduct = products.pop();
+        products.push(lastProduct);
+        let newProduct = {
+            id: lastProduct.id +1,
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            precio: req.body.precio,
+            descuento: req.body.descuento,
+            imagen: req.file.filename
+        }
+
+        products.push(newProduct);
+        let newProductSave = JSON.stringify(products,null,2);
+        fs.writeFileSync(path.resolve(__dirname, '../data/products.json'), newProductSave);
+        res.redirect('/admin');
+    }, 
+    show: (req,res) => {
+        let products = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')));
+        let myProduct;
+        products.forEach(product => {
+            if(product.id == req.params.id) {
+                myProduct = product;
+            }
+            
+        });
+
+        res.render(path.resolve(__dirname, '../views/admin/detail'), {title: "Product", myProduct});
+    }
+
 }
 
 module.exports = adminController;
--- integrador-grupo-12/routes/admin.js
+++ integrador-grupo-12/routes/admin.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const adminController = require('../controllers/adminController');
+const path = require('path');
+const { route } = require('.');
 
-router.get('/admin', adminController.index);
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '../public/imagenes/products'));
+    },
+    filename: function (req, file, cb) {
+      cb(null, 'product-'+Date.now()+path.extname(file.originalname))
+    }
+  })
+   
+  const upload = multer({ storage })
+
+router.get('/', adminController.index);
+router.get('/create', adminController.create);
+router.post('/create', upload.single('imagen'), adminController.save);
+router.get('/detail/:id', adminController.show);
 
 module.exports = router;
